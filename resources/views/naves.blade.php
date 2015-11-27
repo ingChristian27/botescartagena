081f902b22b571d8d2f0f626defaa087fde23df1
christian duban dachiardi <cristianoliveros_27@hotmail.com>
2015-11-26 21:37:26 -0500

    quitar disabled

--- resources/views/naves.blade.php
+++ resources/views/naves.blade.php
@@ -20,7 +20,7 @@
 						  <div class="form-group">
 						    <label for="ejemplo_password_1">Capacidad</label>
 						    <input type="text" class="form-control"  name="capacidad"
-						           placeholder="Contraseña" disabled="">
+						           placeholder="Contraseña" >
 						  </div>
 					</div>
 					<div class="col-lg-12 col-md-12">
